9784b3ba90dcddbbd7240672c0f229c02612c609
Jablan <joshuaablan@gmail.com>
2019-05-25 18:20:42 -0700

    add user conneciton to react-frontend

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import models
 from flask_cors import CORS
 import config
 from flask_login import LoginManager
-# from resources.users import users_api
+from resources.users import users_api
 
 DEBUG = True
 HOST = '0.0.0.0'
@@ -17,20 +17,20 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 
 
-# @login_manager.user_loader
-# def load_user(userid):
-#     try:
-#         return models.User.get(models.User.id == userid)
-#         except models.DoesNotExist:
-#         return None
+@login_manager.user_loader
+def load_user(userid):
+    try:
+        return models.User.get(models.User.id == userid)
+    except models.DoesNotExist:
+        return None
 
 
 # app.use(dogControllr, '/api/v1')
 # url prefix star
 
-# CORS(users_api, origins=["http://localhost:3000"], supports_credentials=True)
+CORS(users_api, origins=["http://localhost:3000"], supports_credentials=True)
 
-# app.register_blueprint(users_api, url_prefix='/users')
+app.register_blueprint(users_api, url_prefix='/users')
 
 
 @app.before_request
@@ -51,5 +51,5 @@ def index():
 
 
 if __name__ == '__main__':
-    # models.initialize()
+    models.initialize()
     app.run(debug=config.DEBUG, port=config.PORT)
